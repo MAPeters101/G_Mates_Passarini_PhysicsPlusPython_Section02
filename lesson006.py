732c826affcada989dfe6d0f8610f449905f553d
Mark Peters <mapeters101@earthlink.net>
2025-01-15 15:39:29 -0500

    Work on lesson006.

---
+++ lesson006.py
@@ -0,0 +1,12 @@
+age = int(input("How old are you? "))
+
+print(age)
+print()
+ph = float(input("What is the pH of the soil? "))
+print(ph)
+print()
+
+name = str(input("What is your name? "))
+print(name)
+print('Your name is',name,', and you are',age,'years old')
+print()
