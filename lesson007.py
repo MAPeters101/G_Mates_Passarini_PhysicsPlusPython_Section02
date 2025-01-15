2620691bc29f6ffb6d3ad52be9beaadfbadb22c6
Mark Peters <mapeters101@earthlink.net>
2025-01-15 15:45:16 -0500

    Work on lesson007 to 0:40.

---
+++ lesson007.py
@@ -0,0 +1,10 @@
+# Create a program that calculates the force given the mass and the
+# acceleration, by using the formula: F = m*a.
+# Then, print:
+# "The mass is X, the acceleration is Y, and the force is Z"
+
+mass = float(input("Please enter the mass: "))
+accel = float(input("Please enter the acceleration: "))
+force = mass * accel
+print(f"The mass is {mass}, the acceleration is {accel}, and the force is {force}")
+
