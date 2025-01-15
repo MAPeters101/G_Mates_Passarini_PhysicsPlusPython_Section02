0ac123cf27273d87ddb49b9c0bb83df2ab02d6c3
Mark Peters <mapeters101@earthlink.net>
2025-01-14 23:47:40 -0500

    Work on lesson004

---
+++ lesson004.py
@@ -0,0 +1,23 @@
+x = 3
+y = 2
+
+print(x)
+print(y)
+
+print('The first number is',x,'and the second number is',y)
+print('The numbers are',x,'and',y)
+
+print(type(x))
+
+n = 4.5
+print(type(n))
+
+phrase = 'I love Python'
+print(type(phrase))
+
+a = True
+b = False
+print(type(a))
+
+z = (1,4,3,True,4.5)
+print(type(z))
