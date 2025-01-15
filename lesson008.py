e85d8c7707b26f3d31020ee62785fc65be5a4cdb
Mark Peters <mapeters101@earthlink.net>
2025-01-15 15:58:34 -0500

    Work on lesson008.

---
+++ lesson008.py
@@ -0,0 +1,9 @@
+# Create a program that calculates the force given the mass and the
+# acceleration, by using the formula: F = m*a.
+# Then, print:
+# "The mass is X, the acceleration is Y, and the force is Z"
+
+m = 2.92
+a = 2.05
+f = m * a
+print('The mass is',m,', the acceleration is',a,', and the force is',f)
