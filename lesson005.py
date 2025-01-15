57e26118c0038f38cee957254b07a7b16616b7f8
Mark Peters <mapeters101@earthlink.net>
2025-01-15 15:33:20 -0500

    Work on lesson005.

---
+++ lesson005.py
@@ -0,0 +1,20 @@
+print(2 + 3)
+print(5 - 3)
+print(3 * 2)
+print(6 / 2)
+print(7 // 3)
+print(-7 // 3)
+print(7 / 3)
+print(7 % 3)
+print(2 ** 3)
+print()
+
+import math
+print(math.sqrt(81))
+print(8**(1/3))
+print()
+
+
+print(2 + 4 * 3 ** 4)
+print((2 + 4) * 3 ** 4)
+
